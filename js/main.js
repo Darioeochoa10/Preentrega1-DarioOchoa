36a6f400d818ea68fd9c8e98d506a2798e93890a
Dario Enrique Ochoa Posse <darioe.upwork@gmail.com>
2023-02-09 21:03:22 -0300

    Segunda entrega terminada

--- js/main.js
+++ js/main.js
@@ -1,24 +1,110 @@
-/* Applicacion que checkea si la persona es admisible para ingresar a las fuerzas policiales*/
+// Vamos a crear un carrito de compras para una herboristeria //
 
-let nacionalidad = prompt("Ingrese su nacionalidad");
-
-if (nacionalidad == "Argentino") {
-  alert("Usted es apto para formar parte de las fuerzas policiales");
-} else {
-  alert("Usted no es apto para ser parte de las fuerzas policiales");
+class Producto {
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
 }
 
-let edad = parseInt(prompt("Ingrese su edad"));
+const carrito = [];
 
-while (edad < 18) {
-  alert("No es apto para ingresar a las fuerzas policiales");
-  edad = parseInt(prompt("Por favor ingrese nuevamentes su edad"));
+const agregarProducto = (producto) => {
+  carrito.push(producto);
+};
+
+const calcularTotal = () => {
+  let total = 0;
+  for (const producto of carrito) {
+    total += producto.precio;
+  }
+  return total;
+};
+
+const teVerde = new Producto("Te verde", 450);
+const teRojo = new Producto("Te rojo", 420);
+const arrozKoshihikari = new Producto("Arroz Koshihikari", 300);
+const algasNori = new Producto("Algas Nori x100", 1200);
+const hierbaMenta = new Producto("Hierba de menta x100gr", 250);
+const hierbaCedron = new Producto("Hierba de cedronx100gr", 300);
+
+alert("¡Bienvenido a la Herboristeria Andante! ¿Qué desea comprar hoy?");
+
+let seguirComprando = true;
+
+while (seguirComprando && seguirComprando != 7) {
+  const productoElegido = prompt(
+    "Elija un producto a agregar al carrito: \n 1. Te verde \n 2. Te Rojo \n 3. Arroz Koshihikari \n 4. Algas Nori x 100 unidades \n 5. Hierba de menta por 100 gramos \n 6. Hierba de cedron por 100 gramos \n 7. Finalizar compra"
+  );
+
+  switch (productoElegido) {
+    case "1":
+      agregarProducto(teVerde);
+      alert(
+        `Usted seleccionó el producto ${teVerde.nombre} con un precio de ${teVerde.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${teVerde.nombre} precio: ${teVerde.precio} pesos argentinos.`
+      );
+      break;
+    case "2":
+      agregarProducto(teRojo);
+      alert(
+        `Usted seleccionó el producto ${teRojo.nombre} con un precio de ${teRojo.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${teRojo.nombre} precio: ${teRojo.precio} pesos argentinos.`
+      );
+      break;
+    case "3":
+      agregarProducto(arrozKoshihikari);
+      alert(
+        `Usted seleccionó el producto ${arrozKoshihikari.nombre} con un precio de ${arrozKoshihikari.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${arrozKoshihikari.nombre} precio: ${arrozKoshihikari.precio} pesos argentinos.`
+      );
+      break;
+    case "4":
+      agregarProducto(algasNori);
+      alert(
+        `Usted seleccionó el producto ${algasNori.nombre} con un precio de ${algasNori.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${algasNori.nombre} precio: ${algasNori.precio} pesos argentinos.`
+      );
+      break;
+    case "5":
+      agregarProducto(hierbaMenta);
+      alert(
+        `Usted seleccionó el producto ${hierbaMenta.nombre} con un precio de ${hierbaMenta.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${hierbaMenta.nombre} precio: ${hierbaMenta.precio} pesos argentinos.`
+      );
+      break;
+    case "6":
+      agregarProducto(hierbaCedron);
+      alert(
+        `Usted seleccionó el producto ${hierbaCedron.nombre} con un precio de ${hierbaCedron.precio} pesos argentinos.`
+      );
+      console.log(
+        `producto seleccionado ${hierbaCedron.nombre} precio: ${hierbaCedron.precio} pesos argentinos.`
+      );
+      break;
+    case "7":
+      seguirComprando = false;
+      alert("Gracias por su compra, lo esperamos nuevamente!");
+      break;
+    default:
+      alert("Por favor, elija una opción válida");
+  }
 }
 
-let estudios = prompt("Posee secundario completo");
-
-if (estudios === "Si") {
-  alert("Usted es apto para formar parte de las fuerzas policiales");
-} else {
-  alert("Usted no es apto para ser parte de las fuerzas policiales");
+let total = 0;
+for (let producto of carrito) {
+  total += producto.precio;
+  console.log(producto.nombre + " - Precio: S/." + producto.precio);
 }
+
+console.log("Total a pagar: S/." + total);
